83ab32dd3d15286fefc1e3e2c32b304c8de4a614
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-06-14 05:05:55 +0530

    app ready

--- vendor/bundle/ruby/2.6.0/gems/devise-4.7.2/lib/devise/models/database_authenticatable.rb
+++ vendor/bundle/ruby/2.6.0/gems/devise-4.7.2/lib/devise/models/database_authenticatable.rb
@@ -32,143 +32,143 @@ module Devise
       extend ActiveSupport::Concern
 
       included do
-        after_update :send_email_changed_notification, if: :send_email_changed_notification?
-        after_update :send_password_change_notification, if: :send_password_change_notification?
+        # after_update :send_email_changed_notification, if: :send_email_changed_notification?
+        # after_update :send_password_change_notification, if: :send_password_change_notification?
 
-        attr_reader :password, :current_password
-        attr_accessor :password_confirmation
+        # attr_reader :password, :current_password
+        # attr_accessor :password_confirmation
       end
 
-      def initialize(*args, &block)
-        @skip_email_changed_notification = false
-        @skip_password_change_notification = false
-        super 
-      end
+      # def initialize(*args, &block)
+      #   @skip_email_changed_notification = false
+      #   @skip_password_change_notification = false
+      #   super 
+      # end
 
       # Skips sending the email changed notification after_update
-      def skip_email_changed_notification!
-        @skip_email_changed_notification = true
-      end
+      # def skip_email_changed_notification!
+      #   @skip_email_changed_notification = true
+      # end
 
-      # Skips sending the password change notification after_update
-      def skip_password_change_notification!
-        @skip_password_change_notification = true
-      end
+      # # Skips sending the password change notification after_update
+      # def skip_password_change_notification!
+      #   @skip_password_change_notification = true
+      # end
 
-      def self.required_fields(klass)
-        [:encrypted_password] + klass.authentication_keys
-      end
+      # def self.required_fields(klass)
+      #   [:encrypted_password] + klass.authentication_keys
+      # end
 
-      # Generates a hashed password based on the given value.
-      # For legacy reasons, we use `encrypted_password` to store
-      # the hashed password.
-      def password=(new_password)
-        @password = new_password
-        self.encrypted_password = password_digest(@password) if @password.present?
-      end
+      # # Generates a hashed password based on the given value.
+      # # For legacy reasons, we use `encrypted_password` to store
+      # # the hashed password.
+      # def password=(new_password)
+      #   @password = new_password
+      #   self.encrypted_password = password_digest(@password) if @password.present?
+      # end
 
-      # Verifies whether a password (ie from sign in) is the user password.
-      def valid_password?(password)
-        Devise::Encryptor.compare(self.class, encrypted_password, password)
-      end
+      # # Verifies whether a password (ie from sign in) is the user password.
+      # def valid_password?(password)
+      #   Devise::Encryptor.compare(self.class, encrypted_password, password)
+      # end
 
-      # Set password and password confirmation to nil
-      def clean_up_passwords
-        self.password = self.password_confirmation = nil
-      end
+      # # Set password and password confirmation to nil
+      # def clean_up_passwords
+      #   self.password = self.password_confirmation = nil
+      # end
 
-      # Update record attributes when :current_password matches, otherwise
-      # returns error on :current_password.
-      #
-      # This method also rejects the password field if it is blank (allowing
-      # users to change relevant information like the e-mail without changing
-      # their password). In case the password field is rejected, the confirmation
-      # is also rejected as long as it is also blank.
-      def update_with_password(params, *options)
-        if options.present?
-          ActiveSupport::Deprecation.warn <<-DEPRECATION.strip_heredoc
-            [Devise] The second argument of `DatabaseAuthenticatable#update_with_password`
-            (`options`) is deprecated and it will be removed in the next major version.
-            It was added to support a feature deprecated in Rails 4, so you can safely remove it
-            from your code.
-          DEPRECATION
-        end
-
-        current_password = params.delete(:current_password)
-
-        if params[:password].blank?
-          params.delete(:password)
-          params.delete(:password_confirmation) if params[:password_confirmation].blank?
-        end
-
-        result = if valid_password?(current_password)
-          update(params, *options)
-        else
-          assign_attributes(params, *options)
-          valid?
-          errors.add(:current_password, current_password.blank? ? :blank : :invalid)
-          false
-        end
-
-        clean_up_passwords
-        result
-      end
-
-      # Updates record attributes without asking for the current password.
-      # Never allows a change to the current password. If you are using this
-      # method, you should probably override this method to protect other
-      # attributes you would not like to be updated without a password.
-      #
-      # Example:
-      #
-        def update_without_password(params, *options)
-          params.delete(:email)
-          super(params)
-        end
-      
-      def update_without_password(params, *options)
-        if options.present?
-          ActiveSupport::Deprecation.warn <<-DEPRECATION.strip_heredoc
-            [Devise] The second argument of `DatabaseAuthenticatable#update_without_password`
-            (`options`) is deprecated and it will be removed in the next major version.
-            It was added to support a feature deprecated in Rails 4, so you can safely remove it
-            from your code.
-          DEPRECATION
-        end
-
-        params.delete(:password)
-        params.delete(:password_confirmation)
-
-        result = update(params, *options)
-        clean_up_passwords
-        result
-      end
-
-      # Destroy record when :current_password matches, otherwise returns
-      # error on :current_password. It also automatically rejects
-      # :current_password if it is blank.
-      def destroy_with_password(current_password)
-        result = if valid_password?(current_password)
-          destroy
-        else
-          valid?
-          errors.add(:current_password, current_password.blank? ? :blank : :invalid)
-          false
-        end
-
-        result
-      end
-
-      # A callback initiated after successfully authenticating. This can be
-      # used to insert your own logic that is only run after the user successfully
-      # authenticates.
-      #
-      # Example:
-      #
-      #   def after_database_authentication
-      #     self.update_attribute(:invite_code, nil)
+      # # Update record attributes when :current_password matches, otherwise
+      # # returns error on :current_password.
+      # #
+      # # This method also rejects the password field if it is blank (allowing
+      # # users to change relevant information like the e-mail without changing
+      # # their password). In case the password field is rejected, the confirmation
+      # # is also rejected as long as it is also blank.
+      # def update_with_password(params, *options)
+      #   if options.present?
+      #     ActiveSupport::Deprecation.warn <<-DEPRECATION.strip_heredoc
+      #       [Devise] The second argument of `DatabaseAuthenticatable#update_with_password`
+      #       (`options`) is deprecated and it will be removed in the next major version.
+      #       It was added to support a feature deprecated in Rails 4, so you can safely remove it
+      #       from your code.
+      #     DEPRECATION
       #   end
-      #
+
+      #   current_password = params.delete(:current_password)
+
+      #   if params[:password].blank?
+      #     params.delete(:password)
+      #     params.delete(:password_confirmation) if params[:password_confirmation].blank?
+      #   end
+
+      #   result = if valid_password?(current_password)
+      #     update(params, *options)
+      #   else
+      #     assign_attributes(params, *options)
+      #     valid?
+      #     errors.add(:current_password, current_password.blank? ? :blank : :invalid)
+      #     false
+      #   end
+
+      #   clean_up_passwords
+      #   result
+      # end
+
+      # # Updates record attributes without asking for the current password.
+      # # Never allows a change to the current password. If you are using this
+      # # method, you should probably override this method to protect other
+      # # attributes you would not like to be updated without a password.
+      # #
+      # # Example:
+      # #
+      #   def update_without_password(params, *options)
+      #     params.delete(:email)
+      #     super(params)
+      #   end
+      
+      # def update_without_password(params, *options)
+      #   if options.present?
+      #     ActiveSupport::Deprecation.warn <<-DEPRECATION.strip_heredoc
+      #       [Devise] The second argument of `DatabaseAuthenticatable#update_without_password`
+      #       (`options`) is deprecated and it will be removed in the next major version.
+      #       It was added to support a feature deprecated in Rails 4, so you can safely remove it
+      #       from your code.
+      #     DEPRECATION
+      #   end
+
+      #   params.delete(:password)
+      #   params.delete(:password_confirmation)
+
+      #   result = update(params, *options)
+      #   clean_up_passwords
+      #   result
+      # end
+
+      # # Destroy record when :current_password matches, otherwise returns
+      # # error on :current_password. It also automatically rejects
+      # # :current_password if it is blank.
+      # def destroy_with_password(current_password)
+      #   result = if valid_password?(current_password)
+      #     destroy
+      #   else
+      #     valid?
+      #     errors.add(:current_password, current_password.blank? ? :blank : :invalid)
+      #     false
+      #   end
+
+      #   result
+      # end
+
+      # # A callback initiated after successfully authenticating. This can be
+      # # used to insert your own logic that is only run after the user successfully
+      # # authenticates.
+      # #
+      # # Example:
+      # #
+      # #   def after_database_authentication
+      # #     self.update_attribute(:invite_code, nil)
+      # #   end
+      # #
       def after_database_authentication
       end
 
@@ -177,22 +177,22 @@ module Devise
       #   encrypted_password[0,29] if encrypted_password
       # end
 
-      if Devise.activerecord51?
-        # Send notification to user when email changes.
-        def send_email_changed_notification
-          send_devise_notification(:email_changed, to: email_before_last_save)
-        end
-      else
-        # Send notification to user when email changes.
-        def send_email_changed_notification
-          send_devise_notification(:email_changed, to: email_was)
-        end
-      end
+      # if Devise.activerecord51?
+      #   # Send notification to user when email changes.
+      #   def send_email_changed_notification
+      #     send_devise_notification(:email_changed, to: email_before_last_save)
+      #   end
+      # else
+      #   # Send notification to user when email changes.
+      #   def send_email_changed_notification
+      #     send_devise_notification(:email_changed, to: email_was)
+      #   end
+      # end
 
-      # Send notification to user when password changes.
-      def send_password_change_notification
-        send_devise_notification(:password_change)
-      end
+      # # Send notification to user when password changes.
+      # def send_password_change_notification
+      #   send_devise_notification(:password_change)
+      # end
 
     protected
 
