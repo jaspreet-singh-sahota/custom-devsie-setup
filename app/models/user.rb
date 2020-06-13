3c7d08347fff992fca4591a67c7fa195187cb8d2
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-06-14 01:34:31 +0530

    Customize only username login

---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+      :authentication_keys => [:username]
+    validates :username, uniqueness: true
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+  
+  # use this instead of email_changed? for Rails = 5.1.x
+  def will_save_change_to_email?
+    false
+  end
+
+   protected
+    def password_required?
+      false
+    end
+
+    def after_confirmation_path_for(resource_name, resource)
+      token = resource.send(:set_reset_password_token)
+      edit_password_path(resource, reset_password_token: token)
+    end
+
+  def valid_password?
+      false
+    end
+end
